bf9623a02b59d0641d323db6fcae3699a93b8bc3
FalseRecord <73399615+FalseRecord@users.noreply.github.com>
2020-12-19 14:10:49 +0000

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -17,25 +17,27 @@ module.exports = {
         return message.channel.send(embed)
     }
 
-    
-    let embed = new discord.MessageEmbed()
-    .setTitle("Action: Shutdown")
-    .setDescription(`Bot shutting down.`)
-    .setColor("#4f7d96")
-    .setFooter(`nil`);
-    
-    return message.channel.send(embed)
-    try {
-        process.exit()
-    } catch(e){
+    if(message.author.id == "700102460286697523"){
 
         let embed = new discord.MessageEmbed()
         .setTitle("Action: Shutdown")
-        .setDescription(`Error shutting bot down: ${e.message}.`)
+        .setDescription(`Bot shutting down.`)
         .setColor("#4f7d96")
         .setFooter(`nil`);
-
-
+        
+        return message.channel.send(embed)
+        try {
+            process.exit()
+        } catch(e){
+    
+            let embed = new discord.MessageEmbed()
+            .setTitle("Action: Shutdown")
+            .setDescription(`Error shutting bot down: ${e.message}.`)
+            .setColor("#4f7d96")
+            .setFooter(`nil`);
+    
+    
+        }
     }
   }
 }
