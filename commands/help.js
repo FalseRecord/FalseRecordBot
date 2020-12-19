751467f5f9a2905214bc178e45529fe3a9012483
FalseRecord <73399615+FalseRecord@users.noreply.github.com>
2020-12-19 13:03:25 +0000

    FixedBugs
    123

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
     execute: (message) => {
         let embed = new discord.MessageEmbed()
         .setTitle("Help Prompt")
-        .setDescription(`\n\n**__MODERATION__**\n!!kick <@user>: kicks player from the server\n!!ban <@user>: Bans user from your discord server\n\n**__FUN__**\n!!ping: Gets the discord bots ping!`)
+        .setDescription(`\n\n**MODERATION**\n/kick <@user>: kicks player from the server\n/ban <@user>: Bans user from your discord server\n\n**FUN**\n/ping: Gets the discord bots ping!`)
         .setColor("#109EE0")
         .setFooter(`Promped by ${message.author.username}`);
 
